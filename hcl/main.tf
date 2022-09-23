1737f481091f80d63a894b5d7d99f24e43a99459
cumet04 <medalhkr@gmail.com>
2022-09-23 19:55:36 +0900

    hcl init

---
+++ hcl/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    // to initialize, $ terraform init -backend-config="bucket=$BACKEND_BUCKET"
+    key    = "sbox_cdktf.terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+module "cdktf" {
+  source = "./modules/cdktf"
+}
