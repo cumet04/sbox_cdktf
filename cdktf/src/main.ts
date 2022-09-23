4f943253ac510a6aa3819b3543a5053ca7773333
cumet04 <medalhkr@gmail.com>
2022-09-23 17:56:35 +0900

    aws env skeleton

--- cdktf/src/main.ts
+++ cdktf/src/main.ts
@@ -1,11 +1,24 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformStack, S3Backend } from "cdktf";
+import * as aws from '@cdktf/provider-aws';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new aws.AwsProvider(this, "AWS", {
+      region: "ap-northeast-1",
+    });
+
+    new S3Backend(this, {
+      bucket: process.env.BACKEND_BUCKET!,
+      key: "sbox_cdktf.terraform.tfstate",
+      region: "ap-northeast-1",
+    });
+
+    new aws.vpc.Vpc(this, 'vpc', {
+      cidrBlock: '10.0.0.0/16',
+    });
   }
 }
 
