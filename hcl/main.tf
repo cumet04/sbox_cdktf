ba81aab7206727453d530cf3c083cdc57ff2d48d
cumet04 <medalhkr@gmail.com>
2022-09-24 15:06:41 +0900

    依存関係の真ん中を抜き出すテスト

--- hcl/main.tf
+++ hcl/main.tf
@@ -15,4 +15,21 @@ terraform {
 
 module "cdktf" {
   source = "./modules/cdktf"
+  sn_public_ids = [aws_subnet.snPublic1.id, aws_subnet.snPublic2.id]
+}
+
+locals {
+  aws_vpc_vpc_id = module.cdktf.vpc_id
+}
+
+resource "aws_subnet" "snPublic1" {
+  availability_zone = "ap-northeast-1a"
+  cidr_block ="10.0.0.0/24"
+  vpc_id = local.aws_vpc_vpc_id
+}
+
+resource "aws_subnet" "snPublic2" {
+  availability_zone = "ap-northeast-1c"
+  cidr_block ="10.0.1.0/24"
+  vpc_id = local.aws_vpc_vpc_id
 }
