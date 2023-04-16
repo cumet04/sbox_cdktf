6948f2a34eea67b1318ebfd642a7b1c37bd17293
inomoto <medalhkr@gmail.com>
2023-04-16 15:04:05 +0900

    sample

--- moved/main.ts
+++ moved/main.ts
@@ -1,11 +1,23 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { File } from "@cdktf/provider-local/lib/file";
+import { LocalProvider } from "@cdktf/provider-local/lib/provider";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new LocalProvider(this, "local", {})
+
+    new File(this, "file1", {
+      filename: "file1.txt",
+      content: "hello world",
+    })
+
+    new File(this, "file2", {
+      filename: "file2.txt",
+      content: "hello world!!",
+    })
   }
 }
 
